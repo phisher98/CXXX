e3edbcac8c55ac55a59d3aab589b35e858c1c346
phisher98 <153359846+phisher98@users.noreply.github.com>
2024-04-27 20:43:11 +0530

    Build b4abea3e2f70346349490c9cfad6a7f7627294c5

---
+++ ixiporn/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 15
+
+cloudstream {
+    authors     = listOf("HindiProvider")
+    language    = "en"
+    description = "IXI Porn"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=uncutmaza.cc&sz=%size%"
+}

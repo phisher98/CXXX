c3c585ae6e43b1797ee473365dca74b36638c603
phisher98 <mandiktest2@gmail.com>
2024-05-09 16:37:27 +0530

    New Provider TollyPro

---
+++ TollyPro/src/main/kotlin/com/TollyPro/TollyProProvider.kt
@@ -0,0 +1,13 @@
+package com.TollyPro
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class TollyProProvider: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(TollyPro())
+        registerExtractorAPI(Ds2play())
+    }
+}

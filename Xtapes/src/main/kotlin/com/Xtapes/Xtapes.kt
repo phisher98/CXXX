eba3814bc7fe20fc99370c398b44636ade3df782
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-16 10:52:55 +0100

    Update Xtapes.kt

--- Xtapes/src/main/kotlin/com/Xtapes/Xtapes.kt
+++ Xtapes/src/main/kotlin/com/Xtapes/Xtapes.kt
@@ -21,7 +21,7 @@ class Xtapes : MainAPI() {
         "61802" to "Naughty America",
         "56050" to "Reality Kings",
         "05415" to "Play Ground",
-        "26761" to "Evil Angle",
+        "11075" to "Evil Angle",
         "11007" to "Harmony Films",
         "40100" to "New Sensations",
         "37001" to "Sweet Sinner",
@@ -30,10 +30,7 @@ class Xtapes : MainAPI() {
         "70587" to "Porn Fidelity",
         "63704" to "Porn World",
         "41606" to "Mofos",
-        "63416" to "Jules Jordan",
-        "47846" to "Big Tits",
-        "10657" to "Teen",
-        "45377" to "MILFS"     
+        "63416" to "Jules Jordan"     
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {

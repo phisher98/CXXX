44e19b7959b088426cf1f04c7c163878f7b4a9a7
Owen <96526481+owencz1998@users.noreply.github.com>
2024-05-16 13:43:37 +0100

    test Xtapes
    seeing if I have done this correctly if not can you show me how you can also do this please.

--- Xtapes/src/main/kotlin/com/Xtapes/Xtapes.kt
+++ Xtapes/src/main/kotlin/com/Xtapes/Xtapes.kt
@@ -30,7 +30,8 @@ class Xtapes : MainAPI() {
         "70587" to "Porn Fidelity",
         "63704" to "Porn World",
         "41606" to "Mofos",
-        "63416" to "Jules Jordan"     
+        "63416" to "Jules Jordan",
+        "category/lesbian-porn-videos" to "Lesbian" 
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {

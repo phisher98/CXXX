0d1f0934dfc3b7f844025b37f59c1898388a6215
Phisher98 <153359846+phisher98@users.noreply.github.com>
2024-08-19 15:56:22 +0530

    Update Extractor.kt

--- TollyPro/src/main/kotlin/com/TollyPro/Extractor.kt
+++ TollyPro/src/main/kotlin/com/TollyPro/Extractor.kt
@@ -2,8 +2,13 @@ package com.TollyPro
 
 import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.ExtractorApi
+import com.lagradost.cloudstream3.extractors.VidhideExtractor
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.getQualityFromName
+
+class Vidsp : VidhideExtractor() {
+    override var mainUrl = "https://vidsp.lol"
+}
 
 open class Ds2play : ExtractorApi() {
     override var name = "DoodStream"

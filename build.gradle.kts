b8d9c19c80337b889fe0ed1cfc98b164dfba7aa8
Phisher98 <153359846+phisher98@users.noreply.github.com>
2025-01-18 17:41:20 +0530

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.6.0")
-        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }

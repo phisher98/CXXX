58faced9dc8ab4d50e7a149cfa503f68354d9be7
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-06-08 22:46:54 +0530

    move Paradisehill

---
+++ Paradisehill/build.gradle.kts
@@ -0,0 +1,25 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    language = "en"
+    // All of these properties are optional, you can safely remove them
+
+     description = "Series porn (use VPN if links not working)"
+     authors = listOf("Sora")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "NSFW",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=en.paradisehill.cc&sz=%size%"
+}

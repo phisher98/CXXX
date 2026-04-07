72ac57002474ca74a8478afffc90bfeddb154c87
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2026-04-07 12:05:27 +0530

    fix error

--- SxyPrn/src/main/kotlin/com/CXXX/SxyPrnProvider.kt
+++ SxyPrn/src/main/kotlin/com/CXXX/SxyPrnProvider.kt
@@ -5,7 +5,6 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.extractors.StreamTape
 import com.lagradost.cloudstream3.extractors.Lulustream1
-import com.lagradost.cloudstream3.extractors.Vidguardto
 import com.lagradost.cloudstream3.extractors.DoodstreamCom
 
 
@@ -16,7 +15,6 @@ class SxyPrnProvider : Plugin() {
         registerMainAPI(SxyPrn())
         registerExtractorAPI(StreamTape())
         registerExtractorAPI(Lulustream1())
-        registerExtractorAPI(Vidguardto())
         registerExtractorAPI(DoodstreamCom())
     }
 }
